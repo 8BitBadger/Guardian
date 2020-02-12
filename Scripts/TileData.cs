5d12bbe02a32fd3de9a9f550e6ea996ab07ab47f
Gerhardt <littlebear101@gmail.com>
2020-02-12 17:02:26 +0200

    update

--- Scripts/TileData.cs
+++ Scripts/TileData.cs
@@ -6,7 +6,9 @@ public class TileData
 //If i have time impliment a leveling system to enable quicker resource creation
 int level;
 //The max amount of people the tile can handle
-interface peopleCap;
-
-
+int peopleCap;
+//The time interval for the generation of the resources, defualt is every second resources are produced
+float productionTick = 1f;
+//woods, mountans, shrines, farms
+//This is work Gerhardt, work now on the game remember your day job suck now get your ass in gear man. I believe in you and yes you are leaving notes for yourself lol.
 }
